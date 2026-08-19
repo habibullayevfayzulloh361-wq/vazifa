1ad58d31ef5f9b1733ac3abfd76e18d69f8469a6
Fayzullo <habibullayevfayzulloh361@gmail.com>
2026-08-19 17:46:42 +0500

    10-vazifa

---
+++ 10-vazifa.py
@@ -0,0 +1,13 @@
+import asyncio
+
+async def a():
+    matn = input("Matn: ")
+
+    result1 = ""
+    i = 0
+    while i < len(matn):
+        if matn[i] not in result1:
+            result1 += matn[i]
+        i += 1
+    print(result1)
+asyncio.run(a())
